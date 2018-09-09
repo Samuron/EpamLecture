25fe27850d17480a86000d47cb18c49cc80e6fd0
Ievgen Makukh <samuronchik@gmail.com>
2018-09-09 13:32:28 +0300

    Adde web hello world sample

---
+++ 01. Hello World/src/HelloWorld.Web/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HelloWorld.Web
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = CreateWebHostBuilder(args);
+            var host = builder.Build();
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return new WebHostBuilder()
+                .UseKestrel(options =>
+                {
+                    options.ListenLocalhost(5000);
+                })
+                .Configure(app =>
+                {
+                    app.Run((httpContext) =>
+                    {
+                        var bytes = Encoding.UTF8.GetBytes("Hello World!");
+                        var response = httpContext.Response;
+                        response.StatusCode = 200;
+                        response.ContentType = "text/plain";
+                        response.ContentLength = bytes.Length;
+
+                        return response.Body.WriteAsync(bytes, 0, bytes.Length);
+                    });
+                });
+        }
+    }
+}
