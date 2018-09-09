128adc725c538af39d93ef6351785dae5fadf4de
Ievgen Makukh <samuronchik@gmail.com>
2018-09-09 12:51:53 +0300

    First example

---
+++ 01. Hello World/src/HelloWorld.Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorld.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
